5436202fd66ab4cb68946fd511d0c00d336e425d
mocos <130593875+MoXcz@users.noreply.github.com>
2025-03-29 20:10:30 -0400

    fix: migrate package to use designated files

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,27 +1,54 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
+	"path/filepath"
 
+	"github.com/MoXcz/go-blog/db/schema"
+	"github.com/pressly/goose/v3"
+	"github.com/pressly/goose/v3/database"
 	_ "modernc.org/sqlite"
 )
 
 func main() {
-	db, err := sql.Open("sqlite", ":memory")
+	ctx := context.Background()
+	db, err := sql.Open("sqlite", "db/schema/foo.db")
 	if err != nil {
 		fmt.Println("Error opening database", err)
 		return
 	}
-	data, err := os.ReadFile("db/schema/schema.sql")
+
+	provider, err := goose.NewProvider(database.DialectSQLite3, db, schema.Embed)
 	if err != nil {
-		fmt.Println("Error reading schema:", err)
-		return
+		log.Fatal(err)
 	}
-	query := string(data)
-	if _, err := db.Exec(query); err != nil {
-		log.Fatalln("Error executing query", err)
+	// List migration sources the provider is aware of.
+	log.Println("\n=== migration list ===")
+	sources := provider.ListSources()
+	for _, s := range sources {
+		log.Printf("%-3s %-2v %v\n", s.Type, s.Version, filepath.Base(s.Path))
+	}
+
+	// List status of migrations before applying them.
+	stats, err := provider.Status(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("\n=== migration status ===")
+	for _, s := range stats {
+		log.Printf("%-3s %-2v %v\n", s.Source.Type, s.Source.Version, s.State)
+	}
+
+	log.Println("\n=== log migration output  ===")
+	results, err := provider.Up(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("\n=== migration results  ===")
+	for _, r := range results {
+		log.Printf("%-3s %-2v done: %v\n", r.Source.Type, r.Source.Version, r.Duration)
 	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.1
 require (
 	github.com/a-h/templ v0.3.857
 	github.com/gosimple/slug v1.15.0
+	github.com/pressly/goose/v3 v3.24.2
 	github.com/yuin/goldmark v1.7.8
 	modernc.org/sqlite v1.36.3
 )
@@ -14,11 +15,15 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gosimple/unidecode v1.0.1 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	golang.org/x/exp v0.0.0-20230315142452-642cacee5cc0 // indirect
+	github.com/sethvargo/go-retry v0.3.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394 // indirect
+	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	modernc.org/libc v1.61.13 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
-	modernc.org/memory v1.8.2 // indirect
+	modernc.org/memory v1.9.1 // indirect
 )
